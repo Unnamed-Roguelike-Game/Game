b1dce8632fd3dc251538b8229109432b21d509ca
cabedev69 <90765517+cabedev69@users.noreply.github.com>
2023-09-12 21:07:36 -0700

    #5 A2.1 Character Health
    Gave player script non-constant Health variable, and made the speed variable non-constant as well. (#150)

--- Unnamed Roguelike Game/Player.gd
+++ Unnamed Roguelike Game/Player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
+var player_speed = 500.0
+
+var player_health = 100
 
 func _physics_process(delta):
 
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
+	velocity = input_direction * player_speed
 
 	move_and_slide()
