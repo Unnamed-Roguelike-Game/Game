fbe831a03724e4680e6e50dd171dd0dbc1da1deb
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-12 23:48:33 -0700

    Tweak character and enemy movement + collisions

--- Unnamed Roguelike Game/Enemy.gd
+++ Unnamed Roguelike Game/Enemy.gd
@@ -3,13 +3,12 @@ extends CharacterBody2D
 @onready var player = $"../../Player"
 @onready var enemy_sprite = $"AnimatedSprite2D"
 
-var ENEMY_SPEED = 10000.0
+var ENEMY_SPEED = 100.0
 
 func _physics_process(delta):
 	
 	var direction_of_player = (player.position - self.position).normalized()
 	
-	velocity.x = direction_of_player.x * ENEMY_SPEED * delta
-	velocity.y = direction_of_player.y * ENEMY_SPEED * delta
+	velocity = (direction_of_player * ENEMY_SPEED * delta)
 	
-	move_and_slide()
+	move_and_collide(velocity)
--- Unnamed Roguelike Game/Player.gd
+++ Unnamed Roguelike Game/Player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-var player_speed = 20000.0
+var player_speed = 300.0
 var player_health = 100
 
 func _physics_process(delta):
 
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = input_direction * player_speed * delta
 	
-	move_and_slide()
+	move_and_collide(velocity)
