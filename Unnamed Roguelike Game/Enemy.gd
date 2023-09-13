7232db581b92ad99fbf126efc626d6a53bd89edf
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-12 23:11:07 -0700

    Misc

--- Unnamed Roguelike Game/Enemy.gd
+++ Unnamed Roguelike Game/Enemy.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
-@onready var player_node = $"../../Player"
+@onready var player = $"../../Player"
+@onready var enemy_sprite = $"AnimatedSprite2D"
+
+var ENEMY_SPEED = 10000.0
 
 func _physics_process(delta):
 	
+	var direction_of_player = (player.position - self.position).normalized()
 	
+	velocity.x = direction_of_player.x * ENEMY_SPEED * delta
+	velocity.y = direction_of_player.y * ENEMY_SPEED * delta
 	
+	move_and_slide()
--- Unnamed Roguelike Game/Player.gd
+++ Unnamed Roguelike Game/Player.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
-var player_speed = 500.0
-
+var player_speed = 20000.0
 var player_health = 100
 
 func _physics_process(delta):
 
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * player_speed
-
+	velocity = input_direction * player_speed * delta
+	
 	move_and_slide()
