c1a4a13254488510669995ffb4d2d504f1336713
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-12 18:07:07 -0700

    Create player scene, allow player to move with WASD or Arrow Keys

---
+++ Unnamed Roguelike Game/Player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+func _physics_process(delta):
+
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * SPEED
+
+	move_and_slide()
