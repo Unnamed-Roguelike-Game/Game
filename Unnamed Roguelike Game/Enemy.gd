b5b457cee6e2f26e498832b06aa437ee71c3f6f2
Joseph <46443751+JRSiwiecki@users.noreply.github.com>
2023-09-13 00:01:23 -0700

    #14 #15 B1.1 B1.2 Add Enemy Movement and Collision (#153)
    * Create enemy scene, add collision and sprite
    
    * Add Area2D for player detection to enemy
    
    * Misc
    
    * Tweak character and enemy movement + collisions
    
    * Remove duplicate variable
    
    * Fix merge issue

---
+++ Unnamed Roguelike Game/Enemy.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@onready var player = $"../../Player"
+@onready var enemy_sprite = $"AnimatedSprite2D"
+
+var ENEMY_SPEED = 100.0
+
+func _physics_process(delta):
+	
+	var direction_of_player = (player.position - self.position).normalized()
+	
+	velocity = (direction_of_player * ENEMY_SPEED * delta)
+	
+	move_and_collide(velocity)
