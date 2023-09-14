5b865d2397c07da2aeece60c0bf1f970e46b5376
cabedev69 <caleb.carroll6@gmail.com>
2023-09-13 17:57:19 -0700

    Switched the hitbox to be on the player instead of the enemy. Created a list in 'Player.gd' that tracks which enemies are in the hitbox using onbodyentered and onbodyexited node signals.

--- Unnamed Roguelike Game/Enemy.gd
+++ Unnamed Roguelike Game/Enemy.gd
@@ -14,7 +14,4 @@ func _physics_process(delta):
 	move_and_collide(velocity)
 
 
-func _on_area_2d_body_entered(body):
-	if body.name == "Player":
-		body.player_health -= 10
-		
+
--- Unnamed Roguelike Game/Player.gd
+++ Unnamed Roguelike Game/Player.gd
@@ -4,6 +4,8 @@ var player_speed = 300.0
 
 @onready var player_health = 100
 
+var enemies_in_hitbox = []
+
 func _physics_process(delta):
 
 	var input_direction = Input.get_vector("left", "right", "up", "down").normalized()
@@ -12,4 +14,14 @@ func _physics_process(delta):
 	move_and_collide(velocity)
 
 
+func _on_area_2d_body_entered(body):
+	if "Enemy" in body.name:
+		player_health -= 10
+		enemies_in_hitbox.push_back(body.name)
+		#print(enemies_in_hitbox)
 
+
+func _on_area_2d_body_exited(body):
+	if "Enemy" in body.name:
+		enemies_in_hitbox.pop_back()
+		#print(enemies_in_hitbox)
