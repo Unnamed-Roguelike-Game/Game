31278210860481789913b5f06d671534b6c74e16
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-14 21:28:41 -0700

    Add types to Ability script

--- Unnamed Roguelike Game/Ability.gd
+++ Unnamed Roguelike Game/Ability.gd
@@ -1,12 +1,12 @@
 extends Label
 
-@onready var player = $"../../Player"
+@onready var player: CharacterBody2D = $"../../Player"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(_delta) -> void:
 	if player != null:
 		text = "Ability Usage Left: " + calculate_ability_usages_left()
 
 
-func calculate_ability_usages_left():
+func calculate_ability_usages_left() -> String:
 	return str(player.max_ability_usage - player.ability_used_count)
