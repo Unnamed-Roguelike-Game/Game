410ddae1f4a6c2b39c915940b57fd5cb7450a4f6
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-14 21:30:33 -0700

    Add types to HP script

--- Unnamed Roguelike Game/HP.gd
+++ Unnamed Roguelike Game/HP.gd
@@ -1,14 +1,9 @@
 extends Label
 
-@onready var player = $"../../Player"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+@onready var player: CharacterBody2D = $"../../Player"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(_delta) -> void:
 	if player != null:
 		text = "HP: " + str(player.get("player_health"))
 	else:
