66d555e00a63f5fe39d4e3bee3bd9c51c932ae8c
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-17 16:51:58 -0700

    Use navigationagents to dictate enemy movement

--- Unnamed Roguelike Game/Scripts/Enemy.gd
+++ Unnamed Roguelike Game/Scripts/Enemy.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
 @onready var player: CharacterBody2D = $"../../../Player"
+@onready var navigation_agent: NavigationAgent2D = $"NavigationAgent2D"
 
 const ENEMY_SPEED: float = 200.0
 
 var current_enemy_health: int = 100
+
+func _ready() -> void:
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+	
+	set_movement_target(player.position)
 
 func _physics_process(delta: float) -> void:
 	if player == null:
@@ -13,8 +20,20 @@ func _physics_process(delta: float) -> void:
 	if current_enemy_health <= 0:
 		queue_free()
 	
-	var direction_of_player: Vector2 = global_position.direction_to(player.global_position).normalized()
+	if navigation_agent.is_navigation_finished():
+		return
+	
+	set_movement_target(player.position)
+	
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+	
+	var new_velocity: Vector2 = (next_path_position - current_agent_position).normalized()
 
-	velocity = (direction_of_player * ENEMY_SPEED * delta)
+	velocity = (new_velocity * ENEMY_SPEED * delta)
 
 	move_and_collide(velocity)
+
+
+func set_movement_target(target_point: Vector2):
+	navigation_agent.target_position = target_point
