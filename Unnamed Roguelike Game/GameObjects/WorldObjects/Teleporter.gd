bddbfd3fcff5d70d0d42642b195ec4bb281fa50b
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-17 20:25:59 -0700

    Prevent player from teleporting to non-existent rooms

--- Unnamed Roguelike Game/GameObjects/WorldObjects/Teleporter.gd
+++ Unnamed Roguelike Game/GameObjects/WorldObjects/Teleporter.gd
@@ -8,6 +8,12 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and room.room_cleared:
 		
 		game.current_room_index += 1
+		
+		if game.current_room_index >= game.MAX_ROOMS:
+			print("u did it")
+			return
+		
 		var target_room = room.get_parent().get_children()[game.current_room_index]
 		var target_room_spawn_point = target_room.get_node("SpawnPoint")
 		body.global_position = target_room_spawn_point.global_position
+		
