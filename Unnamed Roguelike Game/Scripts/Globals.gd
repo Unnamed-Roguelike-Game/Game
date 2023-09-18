13b5e4a3fa9c01db8f843a6863123625397584c3
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-17 20:27:28 -0700

    Remove globals script

--- Unnamed Roguelike Game/Scripts/Globals.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var player_position: Vector2
-
-var max_player_health: int = 100
-var current_player_health: int = 100
-
-var max_ability_usage: int = 5
-var ability_used_count: int = 0
